e514c4c22faf0c8f94f5e33fb915cd5c7fbb8949
Hongdao Meng <108969479+Mycroft-s@users.noreply.github.com>
2025-02-20 19:48:18 -0500

    Update mapper2.py

--- hm1_code/mapper2.py
+++ hm1_code/mapper2.py
@@ -9,8 +9,8 @@ def main(separator='\t'):
         # 第一步输出： word \t count
         word, count_str = line.split(separator)
         count = int(count_str)
-        # 输出： -count \t word
-        print(f"{-count}{separator}{word}")
+        # 输出复合键：-count, word（占位符确保第三个字段存在）
+        print(f"{-count}{separator}{word}{separator}")
 
 if __name__ == "__main__":
     main()
